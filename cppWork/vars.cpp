a18ddd21da70759656f032a06d82e2b860efb4c9
nightfallxyz <real.nightfallxyz@gmail.com>
2025-01-02 12:47:43 -0600

    working on c++

---
+++ cppWork/vars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int kittens = 100;
+    float puppies = 100.8;
+
+    kittens = kittens + 1;
+    puppies = puppies + .87;
+
+    cout << kittens << endl;
+    cout << puppies << endl;
+
+    return 0;
+}
